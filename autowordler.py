ccee1f2ac2cbca4a4e537d87779475526ce569ec
Takafumi Yonekura <yonekura@gmail.com>
2022-01-20 16:18:23 -0800

    fix bug
    and adding letter status

--- autowordler.py
+++ autowordler.py
@@ -1,12 +1,36 @@
 import json
 import random
+import string
+import sys
 
+UNKNOWN = 0
+NOTUSED = 1
+WRONG_PLACE = 2
+MATCHED = 3
+
+letters = {}
+for l in string.ascii_lowercase:
+    letters[l] = UNKNOWN
+
+def get_letter_status():
+    return letters
+
+def get_letter_status_str():
+    result = []
+    for l in string.ascii_lowercase:
+        if letters[l] == UNKNOWN:
+            result.append(l)
+        elif letters[l] == WRONG_PLACE:
+            result.append(l.upper())
+        elif letters[l] == MATCHED:
+            result.append("[" + l.upper() + "]")
+    return "".join(result)
+    
 f = open('file_letters.json', 'r')
 words = json.load(f)
-solution = random.choice(words)
-#print(solution)
 
-def check_word(word):
+def check_word(word, solution):
+    global letters
     assert(len(word) == 5)
     result = []
     tmp_solution = list(solution)
@@ -15,10 +39,16 @@ def check_word(word):
         if letter == tmp_solution[index]:
             result.append(letter.upper())
             tmp_solution[index] = '_'
+            letters[letter] = MATCHED
         elif letter in tmp_solution:
-            if tmp_solution.count(letter) >= word[index+1:].count(letter):
+            if letters[letter] != MATCHED:
+                letters[letter] = WRONG_PLACE
+            if tmp_solution.count(letter) > word[index+1:].count(letter):
                 result.append(letter.lower())
+            else:
+                result.append("_")
         else:
+            letters[letter] = NOTUSED
             result.append("_")
     return(result)
     
@@ -27,7 +57,15 @@ def check_word(word):
 if __name__ == "__main__":
     MAXTRY = 6
     attempt = 1
+    if len(sys.argv) > 1:
+        if sys.argv[1] not in words:
+            print("%s is not in dictionary" % sys.argv[1])
+            sys.exit(1)
+        solution = sys.argv[1]
+    else:
+        solution = random.choice(words)
     while attempt <= MAXTRY:
+        print(get_letter_status_str())
         answer = input()
         answer = answer.lower()
         if len(answer) != 5:
@@ -39,6 +77,6 @@ if __name__ == "__main__":
         if answer == solution:
             print("success! %d/%d" % (attempt, MAXTRY))
             exit()
-        print("".join(check_word(answer)))
+        print("".join(check_word(answer, solution)))
         attempt += 1
     print("failed. answer: %s" % solution)
